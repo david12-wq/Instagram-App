a379b6187623b199c97d9bee2251fc53167b345c
david12-wq <davidkamau245@gmail.com>
2021-01-18 23:12:00 +0300

    add

---
+++ instaApp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Image, Profile, Comments, Likes, Follow
+
+# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Image)
+admin.site.register(Comments)
+admin.site.register(Likes)
+admin.site.register(Follow)
+
+
+
